6433105d48aafbbfae4a23e9aac354f27d640f3c
TheGhostop <anviravat401@gmail.com>
2025-04-03 00:49:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "7818162640:AAGmIkrkd8kxaglbNFBpXxrP7bAL6c2sEOk"
+OWNER_ID = 7046757969  # Your Owner ID
+ADMINS = [OWNER_ID]   # Add other admins if needed
+
+# Telegram API Credentials
+API_ID = 26510660
+API_HASH = "1c5017dbd3cbb417770c19d4a5645fa2"
+
+# Dictionary to store user sessions (admin_id -> list of sessions)
+ADMIN_SESSIONS = {}
+MIN_ACCOUNTS = 50  # Minimum required accounts per admin
